a988032c1998b2c1d2f4c7ed316c61639a6e2e82
IvanTurchinovich <chieser.game@gmail.com>
2021-10-27 11:11:59 +0300

    asd

--- tgBot.py
+++ tgBot.py
@@ -8,8 +8,7 @@ bot = telebot.TeleBot('2018476544:AAEVO50Pzs5DkVQpW270OW55m3hUw7svoqM')
 
 @bot.message_handler(commands=['start','restart'])
 def start(message):
-    mess = f"Hello {message.from_user.first_name}. To enter the name of the store, enter the command /set_name"       
+    mess = f"Hello {message.from_user.first_name}. To enter the name of the store, enter the command"       
     bot.send_message(message.chat.id, mess)
-    create_manager_folder(f'{message.from_user.first_name} {message.from_user.last_name}')
-    
+
 bot.polling(none_stop=True, interval=0)
